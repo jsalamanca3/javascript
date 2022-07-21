9c9e539c75151663b8a107cf1007aa784dab6c73
Jfsalamanca3 <Jfsalamanca3@gmail.com>
2022-07-21 17:09:23 -0500

    Corrección con los punto y comas más una n en el response

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -10,11 +10,11 @@ function fetchData(urlApi, callback) {
     xhttp.onreadystatechange = function (event) {
         if (xhttp.readyState === 4) {
             if(xhttp.status === 200) {
-                callback(null, JSON.parse(xhttp.resposeText))
+                callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                const error = new Error('Error' + urlApi);
+                return callback(error, null);
             }
-        } else {
-            const error = new Error('Error' + urlApi)
-            return callback(error, null);
         }
     }
     xhttp.send();
